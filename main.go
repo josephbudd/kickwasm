78432e782f3892156bfa7cf649e0e12d9b937a96
josephbudd <boejudd@outlook.com>
2019-01-12 14:13:00 -0500

    documentation corrections

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ const (
 	outputFolder       = "output"
 	versionBreaking    = 1 // Each new version breaks backwards compatibility.
 	versionFeature     = 0 // Each new version adds features. Retains backwards compatibility.
-	versionPatch       = 0 // Each new version only fixes bugs. No added features. Retains backwards compatibility.
-	versionDescription = `Ready.`
+	versionPatch       = 1 // Each new version only fixes bugs. No added features. Retains backwards compatibility.
+	versionDescription = `Ready. Made some corrections to documentation.`
 )
 
 var (
